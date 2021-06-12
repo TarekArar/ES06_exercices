36e7017b6b90661b49716e107a570d9aad601c57
TarekArar <gt_arar@esi.dz>
2021-06-12 18:06:05 +0100

    ex6 completed, target a given value in a nested json object based on the key

---
+++ ex6/index.js
@@ -0,0 +1,26 @@
+// 6. Write a JavaScript program to target a given value in a nested JSON object, based on the given key.
+
+function findValueInNestedObject(object, key) {
+  if (object[key]) return object[key];
+  const arrFromObject = Object.values(object).filter(
+    (el) => typeof el === "object"
+  );
+  const value = arrFromObject.map((el) => {
+    return findValueInNestedObject(el, key);
+  });
+  return value[0] || null;
+}
+
+const user = {
+  name: "Tarek",
+  age: 22,
+  profile: {
+    picture: "http://yyyy.yy/yyy",
+    friends: {
+      firstName: "Youcef",
+    },
+  },
+};
+
+console.log(findValueInNestedObject(user, "firstName"));
+console.log(findValueInNestedObject(user, "picture"));
